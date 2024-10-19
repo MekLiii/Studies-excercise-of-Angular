269aed905afa351968dc7e1b1184430c6eaf5657
Kamil Czacharowski <kamil.czacharowski@plumsoftware.pl>
2024-10-19 20:39:53 +0200

    Changed title of page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {AboutComponent} from './components/about/about.component';
+import {InfoComponent} from './components/info/info.component';
 
 export const routes: Routes = [
   {path:"about",component:AboutComponent},
+  {path:"info",component:InfoComponent}
 ];
